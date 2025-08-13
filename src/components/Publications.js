3435361f5dce455d54e9e99ea0173daf35b88391
Taehyung Noh <61936174+yestaehyung@users.noreply.github.com>
2025-08-13 14:05:33 +0900

    Update publications with latest papers and research

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -15,13 +15,51 @@ const Publications = () => {
             <strong>Taehyung Noh</strong>, Seungwan Jin, Haein Yeo, Kyungsik Han
           </p>
           <p className="conference">CIKM 2025 Short</p>
-          <div className="links">
+          {/* <div className="links">
             <a
-              href="https://aclanthology.org/2025.coling-main.382/"
+              href=""
               target="_blank"
               rel="noopener noreferrer"
             >
               Paper
+            </a>
+          </div> */}
+        </div>
+
+        <div className="publication">
+          <h3>
+            LLM-Generated Content-Based Explanations for User Experience in
+            Fashion Recommender Systems
+          </h3>
+          <p className="authors">
+            Haein Yeo, <strong>Taehyung Noh</strong>, Kyungsik Han
+          </p>
+          <p className="conference">Fashion and Textiles</p>
+          {/* <div className="links">
+            <a
+              href=""
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Paper
+            </a>
+          </div> */}
+        </div>
+
+        <div className="publication">
+          <h3>LLM 유해성 공격 전략에 대한 실증적 분석</h3>
+          <p className="authors">
+            Yeajin Shin, Kyungsik Han, <strong>Taehyung Noh</strong>, Mingon
+            Jeong
+          </p>
+          <p className="conference">Fashion and Textiles</p>
+          <div className="links">
+            <a
+              href="https://astlyi.s3.ap-northeast-2.amazonaws.com/2025/TTA_LLM+%E1%84%8B%E1%85%B2%E1%84%92%E1%85%A2%E1%84%89%E1%85%A5%E1%86%BC+%E1%84%80%E1%85%A9%E1%86%BC%E1%84%80%E1%85%A7%E1%86%A8+%E1%84%8C%E1%85%A5%E1%86%AB%E1%84%85%E1%85%A3%E1%86%A8%E1%84%8B%E1%85%A6+%E1%84%83%E1%85%A2%E1%84%92%E1%85%A1%E1%86%AB+%E1%84%89%E1%85%B5%E1%86%AF%E1%84%8C%E1%85%B3%E1%86%BC%E1%84%8C%E1%85%A5%E1%86%A8+%E1%84%87%E1%85%AE%E1%86%AB%E1%84%89%E1%85%A5%E1%86%A8.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Report
             </a>
           </div>
         </div>
